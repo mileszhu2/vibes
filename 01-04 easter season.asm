24ce58c518cc32d9f72d647380021d53e4393301
mileszhu2 <miles.zhu@mail.utoronto.ca>
2026-03-31 18:41:35 -0400

    moved a few things around
    code in main are now functions
    code for the 01-04 is now ready to be put in columns (once menu is ready)

--- 01-04 easter season.asm
+++ 01-04 easter season.asm
@@ -1,7 +1,9 @@
 .data
 displayaddress:     .word       0x10008000
 
-colors: 
+colors: .word 0:6
+
+colors2: 
 .word 0x00ffffca
 .word 0x00ffffcb
 .word 0x00ffffcc
@@ -12,17 +14,30 @@ colors:
 # ...
 
 .text
-lw $t0, displayaddress
-la $t9, colors
-lw $t1, 0($t9)
-sw $t1, 0($t0)
-lw $t1, 4($t9)
-sw $t1, 4($t0)
-lw $t1, 8($t9)
-sw $t1, 8($t0)
-lw $t1, 12($t9)
-sw $t1, 12($t0)
-lw $t1, 16($t9)
-sw $t1, 16($t0)
-lw $t1, 20($t9)
-sw $t1, 20($t0)
+reset_colors:
+    la $t9, colors
+    li $t1, 0 # index
+    li $t2, 0x00ffffc9 # base color
+    reset_colors_loop:
+        beq $t1, 6, reset_colors_done
+        addi $t2, $t2, 1
+        sw $t2, 0($t9)
+        add $t9, $t9, 4
+        addi $t1, $t1, 1
+        j reset_colors_loop
+    reset_colors_done:
+        # display colors
+        lw $t0, displayaddress
+        la $t9, colors
+        lw $t1, 0($t9)
+        sw $t1, 0($t0)
+        lw $t1, 4($t9)
+        sw $t1, 4($t0)
+        lw $t1, 8($t9)
+        sw $t1, 8($t0)
+        lw $t1, 12($t9)
+        sw $t1, 12($t0)
+        lw $t1, 16($t9)
+        sw $t1, 16($t0)
+        lw $t1, 20($t9)
+        sw $t1, 20($t0)
--- columns_m3test.asm
+++ columns_m3test.asm
@@ -66,41 +66,9 @@ to_be_deleted: .word 0:84
 
     # Run the game.
 main:
-    # Initialize the game
-    li $t9, 0x808080 #t9=gray
     lw $t0, ADDR_DSPL # $t0 = base address for display
-    
-    # Initialize the input values for the line drawing code.
-    addi $a0, $zero, 0          # set the X coordinate to 0
-    addi $a1, $zero, 0          # set the Y coordinate to 0
-    addi $a2, $zero, 16         # set the length of the line to 16
-    jal vline_draw              # calls the line drawing function
-    
-    addi $a0, $zero, 7          # set the X coordinate to 7
-    addi $a1, $zero, 0          # set the Y coordinate to 0
-    addi $a2, $zero, 16         # set the length of the line to 16
-    jal vline_draw              # calls the line drawing function
-    
-    addi $a0, $zero, 9          # set the X coordinate to 9
-    addi $a1, $zero, 0          # set the Y coordinate to 0
-    addi $a2, $zero, 5          # set the length of the line to 5
-    jal vline_draw              # calls the line drawing function
-    
-    addi $a0, $zero, 7          # set the X coordinate to 7
-    addi $a1, $zero, 4          # set the Y coordinate to 4
-    addi $a2, $zero, 3          # set the length of the line to 3
-    jal hline_draw              # calls the line drawing function
-    
-    addi $a0, $zero, 0          # set the X coordinate to 0
-    addi $a1, $zero, 0          # set the Y coordinate to 0
-    addi $a2, $zero, 10         # set the length of the line to 10
-    jal hline_draw              # calls the line drawing function
-    
-    addi $a0, $zero, 0          # set the X coordinate to 0
-    addi $a1, $zero, 15         # set the Y coordinate to 15
-    addi $a2, $zero, 8          # set the length of the line to 8
-    jal hline_draw              # calls the line drawing function
-    
+    jal draw_game
+
     # Initialize columns:
     la $s0, next_column
     la $s1, column
@@ -124,52 +92,6 @@ main:
     jal get_random_column
     jal game_loop
     
-    ###
-    ###  Code for drawing a horizontal/vertical line.
-    ###
-    ###  $t0 = location of the top-left corner of the bitmap
-    ###  $a0 = the X coordinate of the start of the line
-    ###  $a1 = the Y coordinate of the start of the line
-    ###  $a2 = the length of the line
-    ###  $t1 = the horizontal offset to add to $t0
-    ###  $t2 = the vertical offset to add to $t0
-    ###  $t3 = the current location in memory of the pixel to draw
-    ###  $t4 = the location of the last pixel in the line
-    
-    hline_draw:
-    sll $t2, $a1, 7                 # Calculate the vertical offset (multiply Y input by 128)
-    sll $t1, $a0, 2                 # Calculate the horizontal offset (multiply X input by 4)
-    add $t3, $t0, $t2               # Add the vertical offset to $t0
-    add $t3, $t3, $t1               # Add the horizontal offset to the location calculated above
-    
-    sll $a2, $a2, 2                 # Calculate the offset from $t3 for the last pixel in the line (multiply $a2 by 4)
-    add $t4, $t3, $a2               # Calculate the position of the last pixel in the line
-    # start of the line-drawing loop
-    hline_loop:
-    beq $t3, $t4, hline_loop_end    # If the current X and Y match the end location of the line, branch out of the loop.
-    sw $t9, 0($t3)                  # Draw a single gray pixel at the current X and Y
-    addi $t3, $t3, 4                # Move to the next pixel in the row
-    j hline_loop                    # Jump to the start of the loop
-    hline_loop_end:
-    jr $ra                          # return statement
-    
-    vline_draw:
-    sll $t2, $a1, 7                 # Calculate the vertical offset (multiply Y input by 128)
-    sll $t1, $a0, 2                 # Calculate the horizontal offset (multiply X input by 4)
-    add $t3, $t0, $t2               # Add the vertical offset to $t0
-    add $t3, $t3, $t1               # Add the horizontal offset to the location calculated above
-    
-    sll $a2, $a2, 7                 # Calculate the offset from $t3 for the last pixel in the line (multiply $a2 by 128)
-    add $t4, $t3, $a2               # Calculate the position of the last pixel in the line
-    # start of the line-drawing loop
-    vline_loop:
-    beq $t3, $t4, hline_loop_end    # If the current X and Y match the end location of the line, branch out of the loop.
-    sw $t9, 0($t3)                  # Draw a single gray pixel at the current X and Y
-    addi $t3, $t3, 128              # Move to the next pixel in the column
-    j vline_loop                    # Jump to the start of the loop
-    vline_loop_end:
-    jr $ra                          # return statement
-
 game_loop:
     # New column turn setup
     jal transfer_next_to_col
@@ -261,17 +183,100 @@ game_loop:
     	jal collision_checker # check collision and update location if applicable
     	jal sleep
     	j key_loop
-    
-    # 2a. Check for collisions
-	# 2b. Update locations (capsules)
-	# 3. Draw the screen
-	# 4. Sleep
 
-    
     j game_loop
     end_game:
     li $v0, 10 # terminate the program gracefully
     syscall
+
+
+
+    ###
+    ###  Code for drawing a horizontal/vertical line.
+    ###
+    ###  $t0 = location of the top-left corner of the bitmap
+    ###  $a0 = the X coordinate of the start of the line
+    ###  $a1 = the Y coordinate of the start of the line
+    ###  $a2 = the length of the line
+    ###  $t1 = the horizontal offset to add to $t0
+    ###  $t2 = the vertical offset to add to $t0
+    ###  $t3 = the current location in memory of the pixel to draw
+    ###  $t4 = the location of the last pixel in the line
+    
+    hline_draw:
+    sll $t2, $a1, 7                 # Calculate the vertical offset (multiply Y input by 128)
+    sll $t1, $a0, 2                 # Calculate the horizontal offset (multiply X input by 4)
+    add $t3, $t0, $t2               # Add the vertical offset to $t0
+    add $t3, $t3, $t1               # Add the horizontal offset to the location calculated above
+    
+    sll $a2, $a2, 2                 # Calculate the offset from $t3 for the last pixel in the line (multiply $a2 by 4)
+    add $t4, $t3, $a2               # Calculate the position of the last pixel in the line
+    # start of the line-drawing loop
+    hline_loop:
+    beq $t3, $t4, hline_loop_end    # If the current X and Y match the end location of the line, branch out of the loop.
+    sw $t9, 0($t3)                  # Draw a single gray pixel at the current X and Y
+    addi $t3, $t3, 4                # Move to the next pixel in the row
+    j hline_loop                    # Jump to the start of the loop
+    hline_loop_end:
+    jr $ra                          # return statement
+    
+    vline_draw:
+    sll $t2, $a1, 7                 # Calculate the vertical offset (multiply Y input by 128)
+    sll $t1, $a0, 2                 # Calculate the horizontal offset (multiply X input by 4)
+    add $t3, $t0, $t2               # Add the vertical offset to $t0
+    add $t3, $t3, $t1               # Add the horizontal offset to the location calculated above
+    
+    sll $a2, $a2, 7                 # Calculate the offset from $t3 for the last pixel in the line (multiply $a2 by 128)
+    add $t4, $t3, $a2               # Calculate the position of the last pixel in the line
+    # start of the line-drawing loop
+    vline_loop:
+    beq $t3, $t4, hline_loop_end    # If the current X and Y match the end location of the line, branch out of the loop.
+    sw $t9, 0($t3)                  # Draw a single gray pixel at the current X and Y
+    addi $t3, $t3, 128              # Move to the next pixel in the column
+    j vline_loop                    # Jump to the start of the loop
+    vline_loop_end:
+    jr $ra                          # return statement
+
+draw_game:
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)
+
+    li $t9, 0x808080 #t9=gray
+    
+    # Initialize the input values for the line drawing code.
+    addi $a0, $zero, 0          # set the X coordinate to 0
+    addi $a1, $zero, 0          # set the Y coordinate to 0
+    addi $a2, $zero, 16         # set the length of the line to 16
+    jal vline_draw              # calls the line drawing function
+    
+    addi $a0, $zero, 7          # set the X coordinate to 7
+    addi $a1, $zero, 0          # set the Y coordinate to 0
+    addi $a2, $zero, 16         # set the length of the line to 16
+    jal vline_draw              # calls the line drawing function
+    
+    addi $a0, $zero, 9          # set the X coordinate to 9
+    addi $a1, $zero, 0          # set the Y coordinate to 0
+    addi $a2, $zero, 5          # set the length of the line to 5
+    jal vline_draw              # calls the line drawing function
+    
+    addi $a0, $zero, 7          # set the X coordinate to 7
+    addi $a1, $zero, 4          # set the Y coordinate to 4
+    addi $a2, $zero, 3          # set the length of the line to 3
+    jal hline_draw              # calls the line drawing function
+    
+    addi $a0, $zero, 0          # set the X coordinate to 0
+    addi $a1, $zero, 0          # set the Y coordinate to 0
+    addi $a2, $zero, 10         # set the length of the line to 10
+    jal hline_draw              # calls the line drawing function
+    
+    addi $a0, $zero, 0          # set the X coordinate to 0
+    addi $a1, $zero, 15         # set the Y coordinate to 15
+    addi $a2, $zero, 8          # set the length of the line to 8
+    jal hline_draw              # calls the line drawing function
+    
+    lw $ra, 0($sp)
+    addi $sp, $sp, 4
+    jr $ra
 
 get_random_column:
     # Generate color combo
